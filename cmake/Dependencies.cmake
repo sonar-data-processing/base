99d35339b5e42446271993af1b789202c2748549
Gustavo Neves <gustavo.neves12@gmail.com>
2016-08-04 09:03:30 -0300

    create configure_file_list macro

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -8,12 +8,22 @@ macro (add_project_dependency PROJECT_NAME SOURCE_PATH BINARY_PATH)
 endmacro()
 
 macro (setup_headers PROJECT_NAME SOURCE_PATH)
-    file ( GLOB HEADERS "${SOURCE_PATH}/*.hpp" )
+    file ( GLOB HEADERS "${SOURCE_PATH}/*.hpp")
     foreach(FILENAME ${HEADERS})
         get_filename_component (HEADER ${FILENAME} NAME)
         configure_file (
             ${FILENAME}
             ${CMAKE_BINARY_DIR}/include/${PROJECT_NAME}/${HEADER}
+            @ONLY)
+    endforeach()
+endmacro()
+
+macro (configure_file_list INPUT_LIST OUTPUT_PATH)
+    foreach(FILEPATH ${INPUT_LIST})
+        get_filename_component (FILENAME ${FILEPATH} NAME)
+        configure_file (
+            ${FILEPATH}
+            ${OUTPUT_PATH}/${FILENAME}
             @ONLY)
     endforeach()
 endmacro()
