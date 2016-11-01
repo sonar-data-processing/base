1ce3e598383af77d819846ddc2c1d7be55b80ae2
gustavoneves12 <gustavo.neves12@gmail.com>
2016-11-01 16:07:26 -0300

    solve recursive problem in the setup_headers macro

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,7 +1,6 @@
 macro (add_project_dependency PROJECT_NAME SOURCE_PATH BINARY_PATH)
     if (NOT TARGET ${PROJECT_NAME})
         add_subdirectory (${SOURCE_PATH}/${PROJECT_NAME} ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME} )
-        setup_headers( ${PROJECT_NAME} ${SOURCE_PATH}/${PROJECT_NAME} )
         setup_headers( ${PROJECT_NAME} ${SOURCE_PATH}/${PROJECT_NAME}/src )
         setup_headers( ${PROJECT_NAME} ${SOURCE_PATH}/${PROJECT_NAME}/include )
     endif()
@@ -16,6 +15,25 @@ macro (setup_headers PROJECT_NAME SOURCE_PATH)
             ${CMAKE_BINARY_DIR}/include/${PROJECT_NAME}/${HEADER}
             @ONLY)
     endforeach()
+
+    file ( GLOB HEADERS "${SOURCE_PATH}/*.h")
+    foreach(FILENAME ${HEADERS})
+        get_filename_component (HEADER ${FILENAME} NAME)
+        configure_file (
+            ${FILENAME}
+            ${CMAKE_BINARY_DIR}/include/${PROJECT_NAME}/${HEADER}
+            @ONLY)
+    endforeach()
+
+    file ( GLOB CHILDREN RELATIVE ${SOURCE_PATH} ${SOURCE_PATH}/[^.]* )
+    list ( REMOVE_ITEM CHILDREN "*.*" )
+
+    foreach (CHILD ${CHILDREN})
+        if ( IS_DIRECTORY ${SOURCE_PATH}/${CHILD} )
+            setup_headers( ${PROJECT_NAME}/${CHILD} ${SOURCE_PATH}/${CHILD})
+        endif()
+    endforeach()
+
 endmacro()
 
 function (configure_file_list)
